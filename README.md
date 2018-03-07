39bdb9a2930c81ce0436840632c4e76f5f8716f5
thelethalcode <kousshikraj.raj@gmail.com>
2018-03-07 15:02:35 +0530

    Task 3 Completed

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # AGV-Task3
+
+This repository contains the codes and files related to the Task3 of AGV
+
+This task is regarding localisation.
+
+In this task we have to combine the bot's sensor inputs and its prior state to  give a more\
+accurate reading.
+
+## Given information
+
+1.The bot is moving with a constant velocity.\
+2.Initial position and velocity are given with a high uncertainity.\
+3.A file with the bot's position as recorded by the sensors after a fixed time step.\
+4.The uncertainity of the initial and the sensor data.\
+5.The timestep after which each data is taken.\
+
+## Applying Kalmann Filter
+
+Let's say the given time step is dt , the initial state is X0, the initial uncertainity is P0.\
+
+Then without the sensor readings, X1 = F*X0 , where F operates on X such that based on its velocity and time step, it gets the next state.  
+
+Therefore P1 = F*P0*F', where F' is F transpose
+
+Let the sensor readings be Z1 and its uncertainity be R.
+
+Let's define a matrix K = P*((P+R)^-1).
+
+Combining the Gausians of both the sensor readings and the state derived from prior knowledge,
+
+X1F = X1 + K*(X1 - Z1)
+P1F = P1 - K*P1
+
+where X1F is a state with a better accuracy than both the initial readings.
+
+Now these act as the prior state for the next data. This process keeps on continuing untill the sensor readings are exhausted.
+
+Each state is more and more accurate than the previous states.
+
+## Running the program
+
+First make sure you have Eigen library installed.
+
+`sudo apt-get install libeigen3-dev`
+
+Then
+
+Clone the repo
+`git clone https://www.github.com/thelethalcode/AGV-Task1.git`\
+
+Cd into the directory
+`cd AGV-Task3`\
+
+Compile the Task
+`g++ Task3.cpp`
+
+You have to enter the text file with the sensor readings as an command line argument. Another optional command line argument is the timestep. If it is not entered the program prompts the user to input it.
+
+`./a.out Data1.txt 1.34`
+
+You will be asked to input the bot's initial coordinates and velocity.
+
+The program prints the bot's positon, velocity and their uncertainity after every time step.
---
+++ Task3.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+#include<eigen3/Eigen/Dense>
+
+#define fi first
+#define se second
+#define ma make_pair
+#define pb push_back
+
+using namespace std;
+using namespace Eigen;
+
+int main(int argc, char const *argv[]) {
+
+  fstream file(argv[1]);
+  if(!file){
+    cout<<"Unable to open file";        //Opening the file
+    return -1;
+  }
+
+  vector< pair<float,float> > Data;
+  while(!file.eof())  {
+    float x,y;
+    file>>x;
+    file.ignore();           // Reading the data from the file and storing it in a vector
+    file>>y;
+    Data.pb(ma(x,y));
+  }
+
+  float time_step;
+  if(argc<=2){
+    cout<<"Enter the time_step"<<endl;
+    cin>>time_step;
+  }                             // Getting the time step
+  else{
+    stringstream temp(argv[2]);
+    temp>>time_step;
+  }
+
+  float Px,Py,Vx,Vy,time1=0;
+  cout<<"Enter initial coordinates :- ";
+  cin>>Px>>Py;                                //Getting the initial dat
+  cout<<"Enter initial x-velocity and y-velocity :-";
+  cin>>Vx>>Vy;
+
+  MatrixXd P(2,2),te_P(2,2),X(2,2),te_X(2,2),R(2,2),F(2,2);
+  te_X<<Px,Py,Vx,Vy;
+  te_P<<1000,0,0,1000;                    // Deg=fining all the necessary matrices
+  R<<0.1,0,0,0.1;
+  F<<1,time_step,0,1;
+
+  cout<<endl<<"INITIALLY \n\nPOSITION = ( "<<Px<<" , "<<Py<<" )"<<endl;
+  cout<<"VELOCITY = "<<Vx<<"i + "<<Vy<<"j"<<endl;
+  cout<<"UNCERTAINITY MATRIX :- \n"<<te_P<<endl;
+  cout<<"============================================================================="<<endl;
+
+  for(vector< pair<float,float> > :: iterator it = Data.begin();it!=Data.end();it++){
+    time1+=time_step;
+    Vx = it->fi - Px;
+    Vx/=time_step;             // Calculating the velocity based on the sensor readings
+    Vy = it->se - Py;
+    Vy/=time_step;
+    Px = it->fi;
+    Py = it->se;
+
+    MatrixXd Z(2,2);               // Defining the matrix for the sensor readings
+    Z<<Px,Py,Vx,Vy;
+
+    X=F*te_X;                // Calculating the new state and uncertainity from prior knowledge
+    P= F*(te_P*(F.transpose()));
+
+    MatrixXd K(2,2),PRi(2,2);
+    PRi = P + R;              // Calculating K
+    K = P*(PRi.inverse());
+
+    X = X + K*(Z-X);         // Combining both the data
+    P = P - K*P;
+
+    te_X = X;             // The prior state acts as the source  for the next state
+    te_P = P;
+
+    // Printing them
+    cout<<fixed<<setprecision(4)<<"AT TIME = "<<time1<<" \n\nPOSITION = ( "<<X(0,0)<<" , "<<X(0,1)<<" )"<<endl;
+    cout<<fixed<<setprecision(4)<<"VELOCITY = "<<X(1,0)<<"i + "<<X(1,1)<<"j"<<endl;
+    cout<<fixed<<setprecision(4)<<"UNCERTAINITY MATRIX :- \n"<<P<<endl<<endl;
+    cout<<"============================================================================="<<endl;
+
+  }
+
+  return 0;
+}
